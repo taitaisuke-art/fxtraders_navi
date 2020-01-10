74aafe45348e22f6c144b97504930922db54b53f
taitaisuke-art <56817737+taitaisuke-art@users.noreply.github.com>
2020-01-10 10:18:53 +0900

    commit2

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,5 +4,19 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
 
 end
